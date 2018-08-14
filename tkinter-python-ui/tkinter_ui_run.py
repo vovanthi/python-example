608d539936a147a0179b27b91626f194b32b75af
vovanthi <vovanthi@earth.sint.co.jp>
2018-08-14 10:00:55 +0900

    delete kivy ui

--- tkinter-python-ui/tkinter_ui_run.py
+++ tkinter-python-ui/tkinter_ui_run.py
@@ -3,11 +3,18 @@ import cv2
 from PIL import Image, ImageTk
 import time
 from tkinter import tix
+import multiprocessing
 
+root = tix.Tk()
+image_label = tix.Label(master=root)
+image_label.pack()
+cam = cv2.VideoCapture(0)
+
+share_dict = multiprocessing.Manager().dict()
+fps = 10
 
 def quit_(root):
     root.destroy()
-
 
 def update_image(image_label, cam):
     (readsuccessful, frame) = cam.read()
@@ -17,15 +24,27 @@ def update_image(image_label, cam):
     image_label._image_cache = b
     root.update()
 
-def update_all(root, image_label, cam, fps):
-    update_image(image_label, cam)
-    root.after(int(3600 / fps), func=lambda: update_all(root, image_label, cam, fps))
+def getCature(share_dict):
+    while True:
+        (readsuccessful, frame) = cam.read()
+        a = Image.fromarray(frame)
+        b = ImageTk.PhotoImage(image=a)
+        share_dict['IMG'] = b
+        sleep(0.1)
+
+def update_image_only(image_label, image):
+    print('updtate image')
+    image_label.configure(image=image)
+    image_label._image_cache = image
+    # root.update()
+
+def update_all(root, image_label, fps):
+    # update_image(image_label, cam)
+    image_label.configure(image=share_dict['IMG'])
+    image_label._image_cache = share_dict['IMG']
+    root.after(int(3600 / fps), func=lambda: update_all(root, image_label, fps))
 
 if __name__ == '__main__':
-    root = tix.Tk()
-    image_label = tix.Label(master=root)
-    image_label.pack()
-    cam = cv2.VideoCapture(0)
-    fps = 60
-    root.after(0, func=lambda: update_all(root, image_label, cam, fps))
+    multiprocessing.Process(target=getCature, args=(share_dict,)).start()
+    root.after(0, func=lambda: update_all(root, image_label, fps))
     root.mainloop()
